2ebb391e100844542a1d20c31657b712eab66d42
Yogesh Chobe <ylchobe@gmail.com>
2009-10-29 02:04:13 -0700

    Initial parsing example. Parse one symbol from input.

--- SinH.hs
+++ SinH.hs
@@ -1,6 +1,15 @@
 module Main where
 import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
 
 main :: IO ()
 main = do args <- getArgs
-          putStrLn ("Hello, " ++ args !! 0)
+          putStrLn (readExpr (args !! 0))
